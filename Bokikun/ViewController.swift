f5877a53db4b8562328ced9032312ac8d0c80596
mike840609 <mike840609@gmail.com>
2017-12-16 11:39:38 +0800

    init

---
+++ Bokikun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bokikun
+//
+//  Created by 蔡鈞 on 16/12/2017.
+//  Copyright © 2017 蔡鈞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
